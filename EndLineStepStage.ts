15ce68abacfe55fe44af5577970719c6350bf713
Anwesh43 <anweshthecool0@gmail.com>
2018-10-18 13:24:12 +0530

    adding ELSNode class

--- EndLineStepStage.ts
+++ EndLineStepStage.ts
@@ -71,3 +71,60 @@ class Animator {
         }
     }
 }
+
+class ELSNode {
+    prev : ELSNode
+    next : ELSNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new ELSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = w / (nodes + 1)
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / 60
+        context.strokeStyle = '#673AB7'
+        context.save()
+        context.translate(gap + this.i * gap, h/2)
+        for(var j = 0; j < 2 ; j++) {
+            const sc : number = Math.min(0.5, Math.max(0, this.state.scale - 0.5 * j)) * 2
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.beginPath()
+            context.moveTo(gap/2 * sc, 0)
+            context.lineTo(gap/2, 0)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : ELSNode {
+        var curr : ELSNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
